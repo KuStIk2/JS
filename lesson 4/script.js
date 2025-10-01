4ae3136b4bbc1b22922691e76fb7a05a58a5c151
Gleb <glebanoha317@gmail.com>
2025-10-02 00:25:17 +0300

    Add files via upload

---
+++ lesson 4/script.js
@@ -0,0 +1,137 @@
+//Задание 1.1
+// a =[1,2,3]
+// alert(a)
+
+// //Задание 1.2
+// const anw = [топор,топ,тап,туп,тип,тэп,ток]
+
+// alert(anw.length)
+
+//Задание 1.3
+
+// a =["a","b","c"]
+// a[0] = 1
+// a[1] = 2
+// a[2] = 3
+// alert(a)
+
+// Задание 2.1
+// j = [1,2,3]
+// j[0]++
+// j[1]++
+// j[2]++
+// alert(j)
+// Задание 2.2
+// const arr = [] 
+// arr[3]='a'
+// arr[8]='b'
+// alert(arr.length)
+
+
+//Задание 2.4
+// const arr = [1,2,3]
+// arr.push(4)
+// arr.push(5)
+// alert(arr)
+
+//Задание 2.5
+// const ara = [1,2,3,4,5]
+// a = ara.shift()
+// b = ara.pop()
+// alert(ara.length)
+
+
+// //задание 3
+// for (let i = 11; i <= 33; i++) {
+// console.log(i);
+// }
+
+//Задание 3.2
+// for (let i = 1; i <= 99; i +=2) {
+// console.log(i);
+// }
+
+//Задание3.3
+// for (let i = 100; i >= 0; i--) {
+// console.log(i);
+// }
+
+//Задание 3.4
+// let num = 2
+// let count = 0
+// while (num <=1000) {
+//     num*=3
+//     count++
+// }
+// console.log(num);
+// console.log(count);
+
+//Задание 4
+// const arr = [2, 5, 9, 15, 1, 4]
+
+// for (let i = 0; i < array.lengt; i++) {
+//     if(arr[i] > 3 && arr[i] < 10){
+//         console.log(arr[i]);
+//     }
+
+//     }
+
+
+//Задание 4.2
+// let sum = 0 
+// for (let index = 2; index < 100; index+= 2) {
+//    sum += index
+    
+// }
+// console.log(sum);
+
+//ЗАдание 4.3
+// const arr = [2, 5, 9, 3, 1, 4];
+// let sum = 0
+// for (let index = 0; index < array.length; index++) {
+//     sum+= arr[i]
+    
+// }
+// console.log(sum);
+
+//Задание 4.4
+// let result = ""
+// for (let i = 1; i < 10; i++) {
+//    result +=`${i}`
+// }
+// console.log(result + "-");
+
+// ЗАдание 4.5
+// const arr = [2, 5, 9, 0, 3, 1, 4]
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+//     if (arr === 0) {
+//         break
+//     }
+// }
+
+//Задание 5
+// const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// for (let i = 0; i < num.length; i++) {
+//  if (num[i] % 2 === 0) {
+//     console.log(num[i] ** 2 );
+//  } else if (num [i] % 3 === 0) {
+//     console.log(num[i] ** 3);
+//  }
+// }
+
+//ЗАдание 5.3
+// const arr = [1, 2, 3, 2, 4, 3, 5, 6, 3, 2, 3];
+// let count = 0
+// for (let i = 0; i < arr.length; i++) {
+//    if (arr[i] === 3) {
+//     count++
+//    }
+    
+// }
+// console.log(count);
+
+//Задание 5.4
+// const arr = [1, 2, 3, 4, 5]
+//  arr.splice(1, 2)
+// console.log(arr);
